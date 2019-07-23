e9aa77538e6e1368be249e9a7883679cbad59f86
AlisonYoon <politescarlett1@gmail.com>
2019-07-23 14:51:36 +0100

    comment types

--- builtinfunc.php
+++ builtinfunc.php
@@ -9,3 +9,16 @@ doThing('Felix', 23);
 echo '<br>';
 //$age parameter has default value. if it doesn't get passed any value in, it will print default value.
 doThing('Matt');
+
+/**
+ *
+ * @param string $name - name of student
+ * @param int $age - age of student
+ * DocBlocks is used to build documentation.
+ *
+ */
+
+/*
+ This is multi line comment.
+  It's different from DocBlock.
+ */
