a5b09f56fa6afd85ceda88e465571fe3fabb7a00
AlisonYoon <politescarlett1@gmail.com>
2019-07-25 16:00:04 +0100

    GET is an associative array so you can print it on the page

--- session/index.php
+++ session/index.php
@@ -1,8 +1,13 @@
+<!DOCTYPE html>
 <?php
 
 
 if(isset($_GET['firstName']) && $_GET['firstName'] === 'sam') {
     echo 'get off the computer, ' . $_GET['lastName'];
+}
+
+if(isset($_GET['address']) && $_GET['address'] === '52') {
+    echo '<br>' . 'your address is: ' . $_GET['address'];
 }
 
 ?>
@@ -12,6 +17,7 @@ if(isset($_GET['firstName']) && $_GET['firstName'] === 'sam') {
 <form action="index.php" method="get">
     First Name: <input type="text" name="firstName"><br>
     Last Name : <input type="text" name="lastName"><br>
+    Address : <input type="text" name="address"><br>
     <input type="submit">
 
 </form>
