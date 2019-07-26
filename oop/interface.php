4d02f92c5486b0020527f4a395784f1ddc955fa6
AlisonYoon <politescarlett1@gmail.com>
2019-07-26 13:47:51 +0100

    interface, polymorphism

--- oop/interface.php
+++ oop/interface.php
@@ -10,12 +10,50 @@ class Sheep implements feedable
 {
     public  function eat(string $food): string    // this has to accept string param as well. otherwise, it throws an error.
     {
-        // TODO: Implement eat() method.
-        return 'Mmmm, lovely ' . $food;
+        if($food !== 'grass'){
+            return 'Mmmm, lovely ' . $food;
+        } else {
+            return 'Get out of here';
+        }
     }
 }
 
-$unpicky = new Sheep();
-echo $unpicky->eat('grass');
+class Pig implements feedable
+{
+    public function eat(string $food): string
+    {
+        return 'Mmmm, lovely ' . $food;
+    }
+
+    public function rollInMud()
+    {
+        return 'YEEEEAHAH';
+    }
+}
+
+$sheeply = new Sheep();
+$piggly = new Pig();
+echo $sheeply->eat('grass');
+
+function makeEat(feedable $farmanimal, string $food):string    //feedable $farmanimal param -> because both Sheep and Pig class implements feedable.
+{
+    return $farmanimal->eat($food);
+}
+
+function rollAround(Pig $pig)
+{
+    echo $pig->rollInMud();
+}
+
+if(rand(0,1) > 0.5) {
+    echo makeEat($piggly, 'rocks');
+} else {
+    echo makeEat($sheeply, 'grass');
+}
 
 // Force a class to do something, use interface
+
+//Polymorphism is in this case, $unpicky and $pickey are. They used same interface.
+//Polymorphism is instances of classes that are forced to have same methods.
+//Polymorphism describes a pattern in Object Oriented Programming in which
+// a class has varying functionality while sharing a common interfaces.
