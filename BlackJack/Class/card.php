658d6202bcdbf70bc7a4ecd40b989ab55667a410
AlisonYoon <politescarlett1@gmail.com>
2019-07-28 18:01:45 +0100

    trying to separate model and controller

--- BlackJack/Class/card.php
+++ BlackJack/Class/card.php
@@ -1,22 +1,5 @@
 <?php
 
-$suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-$ranks = [
-    'ace' => 11,
-    2 => 2,
-    3 => 3,
-    4 => 4,
-    5 => 5,
-    6 => 6,
-    7 => 7,
-    8 => 8,
-    9 => 9,
-    10 => 10,
-    'king' => 10,
-    'queen' => 10,
-    'jack' => 10
-];
-$points = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10];
 
 // Use class to make an array of object instead of associated array for card deck
 class Card
@@ -29,36 +12,25 @@ class Card
     {
         $this->suits = $suits;
         $this->ranks = $ranks;
-        //$this->points = $points;
     }
 
     public function getSuit()
     {
-        return $this->suit;
+        return $this->suits;
     }
 
     public function getRank()
     {
-        return $this->rank;
+        return $this->ranks;
     }
 
-
-    public function generateDeck($suits, $ranks)
-    {
-        foreach($suits as $suit){
-            foreach($ranks as $rank){
-                //foreach($points as $point){
-                $cardDeck[] = [$suit, $rank];
-                //}
-            }
-        }
-        return $cardDeck;
-    }
 }
 
 $newCard = new Card($suits, $ranks);
 var_dump($newCard);
 echo '<br>';
 echo '<br>';
-$resultt = $newCard->generateDeck($suits, $ranks);
-var_dump($resultt);
+var_dump($newCard->getRank());
+echo '<br>';
+echo '<br>';
+var_dump($newCard->getSuit());
