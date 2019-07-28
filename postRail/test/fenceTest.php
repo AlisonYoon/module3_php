afd1180ffc6a7e168c109fcfd4758d2b6d989588
AlisonYoon <politescarlett1@gmail.com>
2019-07-28 22:23:01 +0100

    test Fence unit

---
+++ postRail/test/fenceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+require('../fence.php');
+
+use PHPUnit\Framework\TestCase;
+
+class FenceTest extends TestCase
+{
+    public function testFenceUnits_returnsFence()
+    {
+        // Setup
+        $newFence = new Fence(5,5);
+        $expectedFenceUnits = 4;
+
+        //Execution
+        $resultFenceUnits = $newFence->getFenceUnits();
+
+        echo $resultFenceUnits;
+        //Assertion
+        $this->assertEquals($expectedFenceUnits, $resultFenceUnits);
+    }
+
+}
