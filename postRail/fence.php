8275984c6be6bcf56f86bff1dc5e501a2ed90efe
AlisonYoon <politescarlett1@gmail.com>
2019-07-29 09:03:42 +0100

    Test OK

--- postRail/fence.php
+++ postRail/fence.php
@@ -14,7 +14,7 @@ class Fence
         //$this->rail = $rail;
         //$this->post = $post;
 
-        $this->fenceUnits = min($rail, $post);
+        //$this->fenceUnits = min($rail, $post);
         //fenceUnits : number of railings needed to create a fence
         if($rail < 1 || $post < 2) {
             return 'Too little materials, we cannot build a fence';
--- postRail/test/fenceTest.php
+++ postRail/test/fenceTest.php
@@ -9,8 +9,8 @@ class FenceTest extends TestCase
     public function testFenceUnits_returnsFence()
     {
         // Setup
-        $newFence = new Fence(8,5);
-        $expectedFenceUnits = 4;
+        $newFence = new Fence(18,10);
+        $expectedFenceUnits = 9;
 
         //Execution
         $resultFenceUnits = $newFence->getFenceUnits();
