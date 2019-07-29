a944b79a57704301a0f9edb09ded376cf2f9c662
AlisonYoon <politescarlett1@gmail.com>
2019-07-29 08:13:08 +0100

    changed fence constructor logic

--- postRail/fence.php
+++ postRail/fence.php
@@ -16,6 +16,18 @@ class Fence
 
         $this->fenceUnits = min($rail, $post);
         //fenceUnits : number of railings needed to create a fence
+        if($rail < 1 || $post < 2) {
+            return 'Too little materials, we cannot build a fence';
+        } elseif($post < $rail) {
+            $this->fenceUnits = $post -1;
+            return $this->fenceUnits;
+        } elseif($post > $rail) {
+            $this->fenceUnits = $rail;
+            return $this->fenceUnits;
+        } else {    //$post == $rail
+            $this->fenceUnits = $rail-1;
+            return $this->fenceUnits;
+        }
     }
 
     public function getFenceUnits()
