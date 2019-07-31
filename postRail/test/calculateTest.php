030f648e5157d975e22cad6f956e4157e7eb0d9e
AlisonYoon <politescarlett1@gmail.com>
2019-07-31 22:03:02 +0100

    Cal Amount function unit test

--- postRail/test/calculateTest.php
+++ postRail/test/calculateTest.php
@@ -4,32 +4,54 @@ require('../calculate.php');
 
 use PHPUnit\Framework\TestCase;
 
-class CalculateLengthTest extends TestCase
+//class CalculateLengthTest extends TestCase
+//{
+//    public function testCalculateLength_returnsLength()
+//    {
+//        // Setup
+//        $getLength = new CalLength(3);
+//        $expectedFenceLength = 4.9;
+//        $getLength2 = new CalLength(10);
+//        $expectedFenceLength2 = 16.1;
+//        $getLength3 = new CalLength(0);
+//        $expectedFenceLength3 = -1;
+//        $getLength4 = new CalLength(500000000);
+//        $expectedFenceLength4 = 800000000.1;
+//
+//        //Execution
+//        $resultFenceLength = $getLength->getLength();
+//        $resultFenceLength2 = $getLength2->getLength();
+//        $resultFenceLength3 = $getLength3->getLength();
+//        $resultFenceLength4 = $getLength4->getLength();
+//
+//
+//        //Assertion
+//        $this->assertEquals($expectedFenceLength, $resultFenceLength);
+//        $this->assertEquals($expectedFenceLength2, $resultFenceLength2);
+//        $this->assertEquals($expectedFenceLength3, $resultFenceLength3);
+//        $this->assertEquals($expectedFenceLength4, $resultFenceLength4);
+//
+//    }
+//
+//}
+
+class CalculateAmountTest extends TestCase
 {
-    public function testCalculateLength_returnsLength()
+    public function testCalculateAmount_returnsRailPostAmount()
     {
         // Setup
-        $getLength = new CalLength(3);
-        $expectedFenceLength = 4.9;
-        $getLength2 = new CalLength(10);
-        $expectedFenceLength2 = 16.1;
-        $getLength3 = new CalLength(0);
-        $expectedFenceLength3 = -1;
-        $getLength4 = new CalLength(500000000);
-        $expectedFenceLength4 = 800000000.1;
+        $getAmount = new CalAmount(12.9);
+        $expectedRailPostAmount = ["rail"=>8, "post"=>9];
+
 
         //Execution
-        $resultFenceLength = $getLength->getLength();
-        $resultFenceLength2 = $getLength2->getLength();
-        $resultFenceLength3 = $getLength3->getLength();
-        $resultFenceLength4 = $getLength4->getLength();
+        $resultRailPostAmount = $getAmount->getAmount();
+
 
 
         //Assertion
-        $this->assertEquals($expectedFenceLength, $resultFenceLength);
-        $this->assertEquals($expectedFenceLength2, $resultFenceLength2);
-        $this->assertEquals($expectedFenceLength3, $resultFenceLength3);
-        $this->assertEquals($expectedFenceLength4, $resultFenceLength4);
+        $this->assertEquals($expectedRailPostAmount, $resultRailPostAmount);
+
 
     }
 
