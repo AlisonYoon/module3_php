f015e3b4138ab498df2fe32bb126f43bd6c1acec
AlisonYoon <politescarlett1@gmail.com>
2019-08-01 14:46:54 +0100

    prettified

--- polymorphismRecap/User.php
+++ polymorphismRecap/User.php
@@ -3,24 +3,24 @@
 
 abstract class User
 {
-private $name;
-public $spendTotal;
-public $interests;
+    private $name;
+    public $spendTotal;
+    public $interests;
 
-public function __construct(string $name, int $spendTotal, array $interests = [])
-{
-$this->name = $name;
-$this->spendTotal = $spendTotal;
-$this->interests = $interests;
-}
+    public function __construct(string $name, int $spendTotal, array $interests = [])
+    {
+    $this->name = $name;
+    $this->spendTotal = $spendTotal;
+    $this->interests = $interests;
+    }
 
-public function sayInterests()
-{
-return 'I like ' . $this->interests[0];
-}
+    public function sayInterests()
+    {
+    return 'I like ' . $this->interests[0];
+    }
 
-public function getName()
-{
-return $this->name;
-}
+    public function getName()
+    {
+    return $this->name;
+    }
 }
